b0e38f6cc83b805dc03177e5dd58a786e10fc874
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-10-06 23:43:01 +0100

    Dynamic Web Content: Testing first API request by accessing the status endpoint

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function (response) {
+    if (response.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+
   const amenities = {};
   $('input[type="checkbox"]').click(function () {
     if ($(this).is(':checked')) {
